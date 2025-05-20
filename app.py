a93b0a357a6d67c7a45d217a087f9c2532c89dbd
Joyanto005 <168003454+Joyanto005@users.noreply.github.com>
2025-05-20 16:33:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, Response
-from refresh import update_playlist
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Nirapotta m3u server is running"
-
-@app.route('/playlist.m3u')
-def serve_playlist():
-    update_playlist()
-    with open("output.m3u", "r") as f:
+def serve_m3u():
+    with open('output.m3u', 'r') as f:
         content = f.read()
-    return Response(content, mimetype='audio/x-mpegurl')
+    return Response(content, mimetype='application/x-mpegURL')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run()
