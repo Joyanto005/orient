8d457dfd0513a38c5e7b58ca204c6efbbd53503b
Joyanto005 <168003454+Joyanto005@users.noreply.github.com>
2025-05-20 11:50:47 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, send_file
 from refresh import update_playlist
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def serve_playlist():
-    update_playlist()
-    return send_file('output.m3u', mimetype='application/x-mpegURL')
+def home():
+    return "M3U Auto-Refresh Service is Running"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+@app.route('/output.m3u')
+def serve_m3u():
+    update_playlist()
+    return send_file("output.m3u", mimetype="application/x-mpegURL")
