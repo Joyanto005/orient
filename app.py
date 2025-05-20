0e9be8e206f28a209030617e6575bb8a689bc27e
Joyanto005 <168003454+Joyanto005@users.noreply.github.com>
2025-05-20 09:55:24 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "M3U Server is running!"
+
+@app.route('/playlist.m3u')
+def serve_playlist():
+    return send_file("output.m3u", mimetype="application/x-mpegURL")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
