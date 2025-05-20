1e2fe7b3074e3c2ac956675a8932a5387e8a0225
Joyanto005 <168003454+Joyanto005@users.noreply.github.com>
2025-05-20 11:18:14 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "M3U Server is running!"
-
-@app.route('/playlist.m3u')
-def serve_playlist():
+@app.route("/")
+def serve_m3u():
     return send_file("output.m3u", mimetype="application/x-mpegURL")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
