cb42d13cf64de765c66628ab8a44f0fc8663c0ba
Joyanto005 <168003454+Joyanto005@users.noreply.github.com>
2025-05-20 11:30:18 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_file
+from refresh import update_playlist
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def serve_m3u():
-    return send_file("output.m3u", mimetype="application/x-mpegURL")
+@app.route('/')
+def serve_playlist():
+    update_playlist()
+    return send_file('output.m3u', mimetype='application/x-mpegURL')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
